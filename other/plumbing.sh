7414db1fb8fe0dfbc8d25802b697882bf9527137
lukasz-lobocki <lukasz.lobocki@googlemail.com>
2023-09-20 11:40:07 +0200

    feat: adding colors to plumbing

--- other/plumbing.sh
+++ other/plumbing.sh
@@ -5,26 +5,34 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[0;93m'
+NC='\033[0m' # No Color
+
 REMOTES=$(git remote)
 BRANCH=$(git branch --show-current)
 
-echo "Remotes:"; echo "${REMOTES^^}"; echo
-echo "Branch:"; echo "${BRANCH^^}"; echo
+echo -e "${YELLOW}remotes${NC}";
 
 for REMOTE in $REMOTES; do
-  echo "remote show ${REMOTE^^}"; git remote --verbose show $REMOTE \
-    | sed "/Remote branches:/,/Local ref configured for 'git push':/{/Remote branches:/{N;N;d};/Local ref configured for 'git push'/!d}"; echo;
-done
+  echo -e "${REMOTE^^} ${BRANCH^^}"
+done; echo
 
 for REMOTE in $REMOTES; do
-  echo "fetch ${REMOTE^^} ${BRANCH^^}"; git fetch --dry-run --porcelain --verbose $REMOTE $BRANCH; echo;
-done
+  echo -e "${YELLOW}remote show ${REMOTE^^}${NC}"; git remote --verbose show $REMOTE \
+    | sed "/Remote branches:/,/Local ref configured for 'git push':/{/Remote branches:/{N;N;d};/Local ref configured for 'git push'/!d}";
+done; echo
 
 for REMOTE in $REMOTES; do
-  echo "ls-remote ${REMOTE^^} ${BRANCH^^}"; git ls-remote --heads $REMOTE $BRANCH HEAD \
-    | awk '{print $1}'; echo
+  echo -e "${YELLOW}fetch ${REMOTE^^} ${BRANCH^^}${NC}"; git fetch --dry-run --porcelain --verbose $REMOTE $BRANCH;
+done; echo
+
+for REMOTE in $REMOTES; do
+  echo -e "${YELLOW}ls-remote ${REMOTE^^} ${BRANCH^^}${NC}"; git ls-remote --heads $REMOTE $BRANCH HEAD \
+    | awk '{print $1}';
 done
 
-echo "rev-pasres head LOCAL"; git rev-parse HEAD
-echo "log -1 LOCAL"; git log -1 --oneline --no-abbrev-commit \
+echo -e "${YELLOW}rev-parse head LOCAL${NC}"; git rev-parse HEAD; echo
+echo -e "${YELLOW}log -1 LOCAL${NC}"; git log -1 --oneline --no-abbrev-commit \
   | awk '{print $1}'; echo
